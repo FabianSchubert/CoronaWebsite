ffd7745edcbe60106103a58e0516b480a13f6865
Fabian Schubert <fj.schubert@freenet.de>
2020-06-17 20:24:21 +0200

    .

---
+++ php/index.php
@@ -0,0 +1,4 @@
+<?php
+header('Location: http://itp.uni-frankfurt.de/~gros/');
+?>
+
---
+++ php/visitCounter.php
@@ -0,0 +1,13 @@
+<?php
+$path = 'webCounter_index-main';
+
+$file  = fopen( $path, 'r' );
+$count = fgets( $file, 1000 );
+fclose( $file );
+
+$count = abs( intval( $count ) ) + 1;
+
+$file = fopen( $path, 'w' );
+fwrite( $file, $count );
+fclose( $file );
+?>
